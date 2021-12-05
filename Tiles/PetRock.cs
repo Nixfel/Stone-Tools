e6fe9d25e1f5e73ab0743f0004746acf8d98c25a
Nixfel <95149111+Nixfel@users.noreply.github.com>
2021-12-04 19:36:58 -0700

    Add files via upload

---
+++ Tiles/PetRock.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace StoneTools.Tiles
+{
+	public class PetRock : ModTile
+	{
+		public override void SetDefaults()
+		{
+			Main.tileSolid[Type] = false;
+			Main.tileSolidTop[Type] = false;
+			Main.tileFrameImportant[Type] = true;
+
+			TileObjectData.newTile.CopyFrom(TileObjectData.Style1x1);
+			TileObjectData.newTile.StyleHorizontal = true;
+			TileObjectData.newTile.LavaDeath = false;
+			TileObjectData.addTile(Type);
+
+
+			ModTranslation name = CreateMapEntryName();
+			name.SetDefault("Pet Rock");
+			AddMapEntry(new Color(152, 171, 198), name);
+
+			dustType = 84;
+			drop = ModContent.ItemType<Items.PetRock>();
+			soundType = SoundID.Tink;
+			soundStyle = 1;
+		}
+	}
+}
